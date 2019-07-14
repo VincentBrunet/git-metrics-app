9087d405f83c4a0dfabb6055543e55681b0c9b6f
Vincent Brunet <vincent.brunet@epitech.eu>
2019-07-14 16:38:27 -0700

    keep going

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,3 +1,9 @@
+<template>
+  <div class="comp">
+    <canvas ref="rendering"></canvas>
+  </div>
+</template>
+
 <script>
 // External depts
 import axios from "axios";
@@ -7,105 +13,228 @@ import Component from "@/core/Component";
 // Start building component
 var vue = new Component("Chart");
 
-var url = "http://127.0.0.1:9080/";
+var url = "http://127.0.0.1:7080/";
 
+// Main graph type
 vue.prop("type");
 
-vue.prop("datasets_routes");
-vue.prop("datasets_params");
-vue.prop("datasets_labels");
-vue.prop("datasets_colors");
+// Generic params
+vue.prop("name");
 
-vue.prop("xaxis_format");
-vue.prop("yaxis_format");
+// Categories params
+vue.prop("categories_names");
+vue.prop("categories_colors");
+vue.prop("categories_values");
+vue.prop("categories_points");
+vue.prop("categories_xkey");
+vue.prop("categories_ykey");
+
+// History params
+vue.prop("history_names");
+vue.prop("history_scale");
+vue.prop("history_colors");
+vue.prop("history_points");
+vue.prop("history_xkey");
+vue.prop("history_ykey");
 
 // Init
-vue.data("renderingOptions", null);
-vue.data("renderingCanvas", null);
+vue.data("chart", null);
 vue.mounted(function () {
-  this.renderingOptions = {};
-  this.renderingCanvas = new Chart(this.$refs.rendering, this.renderingOptions);
+  if (this.chart != null) {
+    this.chart.destroy();
+  }
+  this.chart = new Chart(this.$refs.rendering, {});
 });
 
 // Data update
 vue.when(
   [
+    // Base
     "$mounted",
     "type",
-    "datasets_routes",
-    "datasets_params",
-    "datasets_labels",
-    "datasets_colors",
-    "xaxis_format",
-    "yaxis_format",
+    // Categories
+    "categories_names",
+    "categories_colors",
+    "categories_values",
+    "categories_points",
+    "categories_xkey",
+    "categories_ykey",
+    // History
+    "history_names",
+    "history_scale",
+    "history_colors",
+    "history_points",
+    "history_xkey",
+    "history_ykey",
   ],
   async function () {
-
-    if (this.type == "time_blocks") {
-      this.do_time_blocks();
-    }
-
+    this.render();
   }
 );
 
 import Lambdas from "@/core/Lambdas";
 
-vue.method("do_time_blocks", async function () {
+vue.method("render", async function () {
+  if (this.type == "categories") {
+    this.render_categories();
+  }
+  if (this.type == "history") {
+    this.render_history();
+  }
+});
+
+vue.method("render_categories", async function () {
   // Needed
-  var bars = [];
+  var positions = {};
+  var categories = [];
   var datasets = [];
-  // For each endpoint
-  for (var e = 0; e < this.datasets_routes.length; e++) {
+  // Read values
+  if (this.categories_values) {
+    for (var i = 0; i < this.categories_values.length; i++) {
+      var bar = Lambdas.resolve(this.categories_xkey, this.categories_values[i]);
+      categories.push(bar);
+      positions[bar] = i;
+    }
+  }
+  // Read points
+  for (var i = 0; i < this.categories_points.length; i++) {
+    // Dataset values
+    var values = [];
+    // Prepare range
+    for (var j = 0; j < categories.length; j++) {
+      values.push(undefined);
+    }
+    // Route infos
+    var routeUrl = this.categories_points[i][0];
+    var routeParams = this.categories_points[i][1];
     // Request to the API
-    var response = await axios.get(url + this.datasets_routes[e], {
-      params: this.datasets_params[e],
+    var response = await axios.get(url + routeUrl, {
+      params: routeParams,
     });
     // Parse results
-    var results = response.data.results;
+    var points = response.data.results;
     // For each data points
-    var values = [];
-    for (var i = 0; i < results.length; i++) {
-      // Point reading
-      var point = results[i];
-      // Setup bars if first endpoint
-      if (e == 0) {
-        var bar = Lambdas.resolve(this.xaxis_format, point);
-        bars.push(bar);
+    for (var j = 0; j < points.length; j++) {
+      var point = points[j];
+      var bar = Lambdas.resolve(this.categories_xkey, point);
+      var position = positions[bar];
+      if (position === undefined) {
+        categories.push(bar);
+        positions[bar] = values.length;
+        position = values.length;
+        values.push(undefined);
       }
-      // Setup values
-      var value = Lambdas.resolve(this.yaxis_format, point);
-      values.push(value);
+      values[position] = (values[position] || 0) + parseInt(Lambdas.resolve(this.categories_ykey, point));
     }
     // Add one dataset per endpoint
     datasets.push({
       data: values,
-      label: this.datasets_labels && this.datasets_labels[e],
-      backgroundColor: this.datasets_colors && this.datasets_colors[e],
+      label: this.categories_names && this.categories_names[i],
+      backgroundColor: this.categories_colors && this.categories_colors[i],
+      borderColor: this.categories_colors[i],
+      borderWidth: 1,
     });
   }
   // Render
-  this.do_bar(bars, datasets);
+  var chart = {
+    type: "bar",
+    data: {
+      labels: categories,
+      datasets: datasets,
+    },
+    options: {
+      title: {
+        display: true,
+        text: this.name,
+      },
+    },
+  };
+  this.update(chart);
 });
 
-vue.method("do_bar", function (labels, datasets) {
-  this.renderingOptions.type = "bar";
-  this.renderingOptions.data = {
-    labels: labels,
-    datasets: datasets,
+vue.method("render_history", async function () {
+  // Make datasets
+  var datasets = [];
+  // Read points
+  for (var i = 0; i < this.history_points.length; i++) {
+    // Route infos
+    var routeUrl = this.history_points[i][0];
+    var routeParams = this.history_points[i][1];
+    // Request to the API
+    var response = await axios.get(url + routeUrl, {
+      params: routeParams,
+    });
+    // Parse results
+    var points = response.data.results;
+    var values = [];
+    var times = {};
+    // For each data points
+    for (var j = 0; j < points.length; j++) {
+      var point = points[j];
+      var x = new Date(Lambdas.resolve(this.history_xkey, point));
+      var y = parseInt(Lambdas.resolve(this.history_ykey, point));
+      if (times[x] === undefined) {
+        var value = {
+          x: x,
+          y: y,
+        };
+        times[x] = value;
+        values.push(value);
+      }
+      else {
+        times[x].y += y;
+      }
+    }
+    // Add one dataset per endpoint
+    datasets.push({
+      data: values,
+      label: this.history_names && this.history_names[i],
+      backgroundColor: this.history_colors && this.history_colors[i],
+      borderColor: this.history_colors[i],
+      borderWidth: 1,
+    });
+  }
+  // Render
+  var chart = {
+    type: "bar",
+    data: {
+      datasets: datasets,
+    },
+    options: {
+      title: {
+        display: true,
+        text: this.name,
+      },
+      scales: {
+        xAxes: [{
+          type: 'time',
+          distribution: "linear",
+          bounds: "data",
+          offset: true,
+          time: {
+            unit: this.history_scale,
+          },
+          ticks: {
+            autoSkip: true,
+            source: "auto",
+          }
+        }],
+      },
+    },
   };
-  this.renderingOptions.options = {};
-  this.renderingCanvas.update();
+  this.update(chart);
+});
+
+vue.method("update", async function (chart) {
+  if (this.chart != null) {
+    this.chart.destroy();
+  }
+  this.chart = new Chart(this.$refs.rendering, chart);
 });
 
 // Export
 export default vue.export();
 </script>
-
-<template>
-  <div class="comp">
-    <canvas ref="rendering"></canvas>
-  </div>
-</template>
 
 <style scoped>
 .comp {
--- src/pages/Repository.vue
+++ src/pages/Repository.vue
@@ -10,14 +10,92 @@ vue.computed("id", function () {
   return this.$route.params.id;
 });
 
-vue.data("data", "hello");
+vue.computed("tz", function () {
+  // @TODO - Guess timezone
+  return 'America/Los_Angeles';
+})
 
-vue.prop("prop", "default");
+vue.computed("charts", function () {
 
-vue.data("authors", [26, 30, 31, 35]);
+  var charts = [];
 
-vue.when(["$mounted", "id"], function () {
+  var frames = [
+    ["All years", "year", "{{time_year}}", "year"],
+    ["All quarters", "quarter", "{{time_quarter}}", "quarter"],
+    ["All months", "month", "{{time_month}}", "month"],
+    ["All weeks", "week", "{{time_week}}", "week"],
+    ["All days", "day", "{{time_day}}", "day"],
+  ];
+  for (var i = 0; i < frames.length; i++) {
+    var frame = frames[i];
+    charts.push({
+      type: "history",
+      name: frame[0],
+      scale: frame[3],
+      points: [
+        ['commit/count', {'git_repository_ids': [this.id], 'timezone': this.tz, 'timeframe': frame[1]}],
+        ['commit/count', {'git_repository_ids': [this.id], 'timezone': this.tz, 'timeframe': frame[1], 'git_author_ids': this.authors}],
+      ],
+      xkey: frame[2],
+      ykey: "{{count}}",
+    });
+  }
+
+  var chunks = [
+    ["Time of the week", ["dow", "hod(6)"], "{{time_dow}} {{time_hod(6)}}:00"],
+    ["Day of the month", ["dow"], "{{time_dow}}"],
+    ["Hour of day", ["hod"], "{{time_hod}}:00"],
+    ["Day of month", ["dom"], "Day {{time_dom}}"],
+    ["Day of year", ["doy"], "Day {{time_doy}}"],
+    ["Month of year", ["moy"], "{{time_moy}}"],
+  ];
+  chunks[0].push([]);
+  for (var i = 0; i < 7; i++) {
+    for (var j = 0; j < 24/6; j++) {
+      chunks[0][3].push({time_dow: i, "time_hod(6)": j*6});
+    }
+  }
+  chunks[1].push([]);
+  for (var i = 0; i < 7; i++) {
+    chunks[1][3].push({time_dow: i});
+  }
+  chunks[2].push([]);
+  for (var i = 0; i < 24; i++) {
+    chunks[2][3].push({time_hod: i});
+  }
+  chunks[3].push([]);
+  for (var i = 1; i <= 31; i++) {
+    chunks[3][3].push({time_dom: i});
+  }
+  chunks[4].push([]);
+  for (var i = 1; i <= 366; i++) {
+    chunks[4][3].push({time_doy: i});
+  }
+  chunks[5].push([]);
+  for (var i = 1; i <= 12; i++) {
+    chunks[5][3].push({time_moy: i});
+  }
+  for (var i = 0; i < chunks.length; i++) {
+    var chunk = chunks[i];
+    charts.push({
+      type: "categories",
+      name: chunk[0],
+      values: chunk[3],
+      points: [
+        ['commit/count', {'git_repository_ids': [this.id], 'timezone': this.tz, 'timechunks': chunk[1]}],
+        ['commit/count', {'git_repository_ids': [this.id], 'timezone': this.tz, 'timechunks': chunk[1], 'git_author_ids': this.authors}],
+      ],
+      xkey: chunk[2],
+      ykey: "{{count}}",
+    });
+  }
+
+  return charts;
+
 });
+
+vue.data("authors", [2, 17, 21, 38, 39, 116]);
+//vue.data("authors", [26, 30, 31, 35]);
 
 vue.component("chart", Chart);
 
@@ -32,166 +110,41 @@ export default vue.export();
     Chart:
 
     <chart
-      :type="'time_blocks'"
 
-      :datasets_routes="[
-        'commit/count',
-        'commit/count',
+      v-for="chart in charts"
+
+      :type="chart.type"
+      :name="chart.name"
+
+      :categories_names="[
+        'Authors: All',
+        'Authors: ' + JSON.stringify(authors),
       ]"
-      :datasets_params="[
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dow', 'hod']},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dow', 'hod'], 'git_author_ids': authors},
-      ]"
-      :datasets_labels="[
-        'Commits in an average day',
-        'Specific to vincent?',
-      ]"
-      :datasets_colors="[
+      :categories_colors="[
         '#eeeeee',
         '#9999ff',
       ]"
+      :categories_xkey="chart.xkey"
+      :categories_ykey="chart.ykey"
+      :categories_values="chart.values"
+      :categories_points="chart.points"
 
-      :xaxis_format="'{{time_dow}} {{time_hod}}:00'"
-      :yaxis_format="'{{count}}'"
-    >
-    </chart>
-
-
-    <chart
-      :type="'time_blocks'"
-
-      :datasets_routes="[
-        'commit/count',
-        'commit/count',
+      :history_names="[
+        'Authors: All',
+        'Authors: ' + JSON.stringify(authors),
       ]"
-      :datasets_params="[
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dow']},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dow'], 'git_author_ids': authors},
-      ]"
-      :datasets_labels="[
-        'Commits in an average day',
-        'Specific to vincent?',
-      ]"
-      :datasets_colors="[
+      :history_colors="[
         '#eeeeee',
         '#9999ff',
       ]"
+      :history_scale="chart.scale"
+      :history_points="chart.points"
+      :history_xkey="chart.xkey"
+      :history_ykey="chart.ykey"
 
-      :xaxis_format="'{{time_dow}}'"
-      :yaxis_format="'{{count}}'"
     >
     </chart>
 
-    <chart
-      :type="'time_blocks'"
-      :datasets_routes="[
-        'commit/count',
-        'commit/count',
-        'commit/count',
-        'commit/count',
-      ]"
-      :datasets_params="[
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['hod']},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['hod'], 'git_author_ids': [28]},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['hod'], 'git_author_ids': [26, 30, 31, 35]},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['hod'], 'git_author_ids': [5, 8, 37]},
-      ]"
-      :datasets_labels="[
-        'Commits in an average day',
-        'Specific to Jacqui',
-        'Specific to Ethan',
-        'Specific to Ty',
-      ]"
-      :datasets_colors="[
-        '#eeeeee',
-        '#9999ff',
-        '#99ff99',
-        '#ff9999',
-      ]"
-
-      :xaxis_format="'{{time_hod}}:00'"
-      :yaxis_format="'{{count}}'"
-    >
-    </chart>
-
-    <chart
-      :type="'time_blocks'"
-
-      :datasets_routes="[
-        'commit/count',
-        'commit/count',
-        'commit/count',
-      ]"
-      :datasets_params="[
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dom']},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dom'], 'git_author_ids': [28]},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['dom'], 'git_author_ids': [26, 30, 31, 35]},
-      ]"
-      :datasets_labels="[
-        'Commits in an average month',
-        'Specific to 1',
-        'Specific to 2',
-      ]"
-      :datasets_colors="[
-        '#eeeeee',
-        '#9999ff',
-        '#99ff99',
-      ]"
-
-      :xaxis_format="'{{time_dom}}'"
-      :yaxis_format="'{{count}}'"
-    >
-    </chart>
-
-    <chart
-      :type="'time_blocks'"
-
-      :datasets_routes="[
-        'commit/count',
-        'commit/count',
-      ]"
-      :datasets_params="[
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['doy']},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['doy'], 'git_author_ids': authors},
-      ]"
-      :datasets_labels="[
-        'Commits in an average year',
-        'Specific to vincent?',
-      ]"
-      :datasets_colors="[
-        '#eeeeee',
-        '#9999ff',
-      ]"
-
-      :xaxis_format="'Day {{time_doy}}'"
-      :yaxis_format="'{{count}}'"
-    >
-    </chart>
-
-    <chart
-      :type="'time_blocks'"
-
-      :datasets_routes="[
-        'commit/count',
-        'commit/count',
-      ]"
-      :datasets_params="[
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['moy']},
-        {'timezone': 'America/Los_Angeles', 'timechunks': ['moy'], 'git_author_ids': authors},
-      ]"
-      :datasets_labels="[
-        'Commits per months',
-        'Specific to vincent?',
-      ]"
-      :datasets_colors="[
-        '#eeeeee',
-        '#9999ff',
-      ]"
-
-      :xaxis_format="'{{time_moy}}'"
-      :yaxis_format="'{{count}}'"
-    >
-    </chart>
   </div>
 </template>
 
